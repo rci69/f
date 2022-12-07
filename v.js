8cae6cc74ea4d221b8ba794bcca3a618946b7a34
Bruce Wayne <96730813+rci69@users.noreply.github.com>
2022-12-07 18:03:51 +0600

    Update and rename a.js to v.js

--- v.js
+++ v.js
@@ -1,33 +1,28 @@
-var link, label, inh;
-window.onload = function()
-{
-    link = document.getElementById("MenuTreeViewt2");
-    link.onclick = play;
-    link.setAttribute("href", "#")
-}
+var label, inh;
+const style = document.createElement("style")
+style.textContent = "a { pointer-events: none; }"
+document.head.appendChild(style)
+document.body.onclick = function(){play()};
 
 function play()
 {
     label = document.getElementById("hall_slider");
     inh = label.innerHTML;
     label.innerHTML = `
-    <video id="video1" width="750" onended="reset()">
+    <video id="video1" width="740" onended="reset()">
     <source src="https://raw.githubusercontent.com/rci69/f/main/Maximizing%20Alphaness.mp4" type="video/mp4">
     Your browser does not support HTML5 video.
     </video>`
     var myVideo=document.getElementById("video1"); 
-
-    if(myVideo.muted)
-        myVideo.muted = false;
-    if(myVideo.hidden)
-        myVideo.hidden = false;
+    myVideo.muted = false;
+    myVideo.hidden = false;
     if (myVideo.paused) 
         myVideo.play();
-    link.onclick = function(){};
+    document.body.onclick = function(){};
 }
 
 function reset()
 {
     label.innerHTML = inh;
-    link.setAttribute("href", "Student/frmStudentMealOnOff.aspx");
+    style.parentNode.removeChild(style);
 }
